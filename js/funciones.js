8a65b3ed236cba02e14466ede014cafd8a301ede
Josuesinni <josueanimations@gmail.com>
2020-05-06 19:54:22 -0700

    agregue las funciones restantes

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,7 @@
 function div(){
     var txtnum1 = document.getElementById("n1").value;
     var txtnum2 = document.getElementById("n2").value;
+    //Validación    
     if (txtnum1=="Primer número" && txtnum2=="Segundo número") {
         var inputNombre = document.getElementById("n3");
         inputNombre.value = "Resultado";
@@ -11,11 +12,45 @@ function div(){
         inputNombre.value = division;
     }
 }
-  function rest(){
-    var txtnum1 = document.getElementById("n1").value;
-    var txtnum2 = document.getElementById("n2").value;    
+function rest(){
+  var txtnum1 = document.getElementById("n1").value;
+  var txtnum2 = document.getElementById("n2").value;
+  //Validación    
+  if (txtnum1=="Primer número" && txtnum2=="Segundo número") {
+    var inputNombre = document.getElementById("n3");
+    inputNombre.value = "Resultado";
+  }else{
     var resta;
     resta = txtnum1-txtnum2;
     var inputNombre = document.getElementById("n3");
     inputNombre.value = resta;
   }
+}
+function sum(){
+  var txtnum1 = document.getElementById("n1").value;
+  var txtnum2 = document.getElementById("n2").value;
+  //Validación    
+  if (txtnum1=="Primer número" && txtnum2=="Segundo número") {
+    var inputNombre = document.getElementById("n3");
+    inputNombre.value = "Resultado";
+  }else{
+    var resta;
+    resta = txtnum1+txtnum2;
+    var inputNombre = document.getElementById("n3");
+    inputNombre.value = resta;
+  }
+}
+function mult(){
+  var txtnum1 = document.getElementById("n1").value;
+  var txtnum2 = document.getElementById("n2").value;
+  //Validación    
+  if (txtnum1=="Primer número" && txtnum2=="Segundo número") {
+    var inputNombre = document.getElementById("n3");
+    inputNombre.value = "Resultado";
+  }else{
+    var resta;
+    resta = txtnum1*txtnum2;
+    var inputNombre = document.getElementById("n3");
+    inputNombre.value = resta;
+  }
+}
